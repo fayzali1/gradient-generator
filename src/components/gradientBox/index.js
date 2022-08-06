6d62b9ecdc31fa0a8a95ddc66dff9ed8e40c4cef
fayzali1 <alyfayz19@gmail.com>
2022-08-06 15:13:26 +0500

    add gradient into gradient display box

--- src/components/gradientBox/index.js
+++ src/components/gradientBox/index.js
@@ -1,8 +1,19 @@
-import { Box, makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 
-const GradientBox = () => {
+const GradientBox = ({ formik }) => {
   const classes = useStyles();
-  return <Box className={classes.gradientBox} />;
+  const { primaryColor, secondaryColor, angle, percentage } = formik.values;
+
+  return (
+    <div
+      className={classes.gradientBox}
+      style={{
+        background: `linear-gradient(${angle}deg,${primaryColor} ${percentage}%,${secondaryColor} ${
+          100 - percentage
+        }%)`,
+      }}
+    />
+  );
 };
 
 export default GradientBox;
@@ -10,7 +21,6 @@ export default GradientBox;
 const useStyles = makeStyles(() => ({
   gradientBox: {
     height: 150,
-    backgroundColor: "#fff",
     width: 300,
     borderRadius: 10,
   },
